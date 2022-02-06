487bec98bfec8ca59614835b95e3aa422aa21daf
Caio Luis <caioluis40@gmail.com>
2022-02-05 23:00:18 -0300

    removed fake use case for tests

--- app/src/test/java/com/caioluis/receitas/presentation/structure/RecipesInteractorTest.kt
+++ app/src/test/java/com/caioluis/receitas/presentation/structure/RecipesInteractorTest.kt
@@ -1,15 +1,14 @@
 package com.caioluis.receitas.presentation.structure
 
-import com.caioluis.receitas.domain.usecase.FakeUseCase
 import com.caioluis.receitas.domain.usecase.GetRecipesByIngredientsUseCase
 import io.reactivex.Observable
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
 import org.mockito.junit.MockitoJUnitRunner
-import org.mockito.Mockito.`when` as whenever
 
 @RunWith(MockitoJUnitRunner::class)
 class RecipesInteractorTest {
@@ -23,14 +22,17 @@ class RecipesInteractorTest {
     fun setup() {
         getRecipesByIngredientsUseCase = mock(GetRecipesByIngredientsUseCase::class.java)
         interactor = RecipesInteractorImpl(getRecipesByIngredientsUseCase)
+
+        `when`(getRecipesByIngredientsUseCase.invoke(ingredients = listOf()))
+            .thenReturn(
+                Observable.just(
+                    RecipesEffect.ShowRecipes(emptyList())
+                )
+            )
     }
 
     @Test
     fun `when invoke interactor with SearchRecipes command should return ShowRecipes effect`() {
-        val getRecipesByIngredientsUseCaseTest = FakeUseCase(RecipesEffect.ShowRecipes(emptyList()))
-
-        val interactor = RecipesInteractorImpl(getRecipesByIngredientsUseCaseTest)
-
         val response = interactor.invoke(
             RecipesState(),
             RecipesCommand.SearchRecipes(listOf())
@@ -41,34 +43,11 @@ class RecipesInteractorTest {
 
     @Test
     fun `when invoke interactor with AddIngredient command should return AddIngredient effect`() {
-        val getRecipesByIngredientsUseCaseTest = FakeUseCase(RecipesEffect.AddIngredient(""))
-        val interactor = RecipesInteractorImpl(getRecipesByIngredientsUseCaseTest)
-
         val response = interactor.invoke(
             RecipesState(),
             RecipesCommand.AddIngredient("")
         )
 
         response.test().assertResult(RecipesEffect.AddIngredient(""))
-    }
-
-    @Test
-    fun `when invoke interactor with SearchRecipes command should return ShowRecipes effect (mockito)`() {
-
-        // Exemplo com Mockito
-
-        whenever(getRecipesByIngredientsUseCase.invoke(ingredients = listOf()))
-            .thenReturn(
-                Observable.just(
-                    RecipesEffect.ShowRecipes(emptyList())
-                )
-            )
-
-        val response = interactor.invoke(
-            RecipesState(),
-            RecipesCommand.SearchRecipes(listOf())
-        )
-
-        response.test().assertResult(RecipesEffect.ShowRecipes(emptyList()))
     }
 }
