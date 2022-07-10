57cde492e46bbe75f2cbf98c501321fd1eb1f4f0
Caio Luis <caioluis40@gmail.com>
2022-07-09 23:01:36 -0300

    created notification module

--- app/src/main/java/com/caioluis/receitas/di/AppComponent.kt
+++ app/src/main/java/com/caioluis/receitas/di/AppComponent.kt
@@ -2,13 +2,14 @@ package com.caioluis.receitas.di
 
 import com.caioluis.receitas.RecipesApplication
 import com.caioluis.receitas.data.di.DataModule
+import com.caioluis.receitas.notification.di.NotificationModule
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjector
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [RecipesModule::class, AppModule::class, DataModule::class])
+@Component(modules = [RecipesModule::class, AppModule::class, DataModule::class, NotificationModule::class])
 interface AppComponent : AndroidInjector<RecipesApplication> {
     @Component.Factory
     interface Factory {
---
+++ notification/bridge/src/main/java/com/caioluis/receitas/notifications/bridge/NotificationHelper.kt
@@ -0,0 +1,5 @@
+package com.caioluis.receitas.notifications.bridge
+
+interface NotificationHelper {
+    fun dispatchNotification()
+}
